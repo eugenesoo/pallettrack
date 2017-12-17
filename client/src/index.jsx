3b71af5430b4ebc0baded1146154f663d9136adf
Eugene Soo <20952179+eugenesoo@users.noreply.github.com>
2017-12-17 21:57:37 +0900

    fix babel issues and initialise client files

---
+++ client/src/index.jsx
@@ -0,0 +1,10 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const App = () => (
+  <div>
+    Hello World!
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
