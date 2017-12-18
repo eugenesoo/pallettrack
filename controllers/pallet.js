be0d86657b3a5861b5c0f01b29ffad55f7908af2
Eugene Soo <20952179+eugenesoo@users.noreply.github.com>
2017-12-18 10:41:33 +0900

    fix insert pallet ajax

--- controllers/pallet.js
+++ controllers/pallet.js
@@ -1,11 +1,11 @@
 const { pgClient } = require('../database');
 
 const insertPallet = (palletName, palletPart, palletQty) => {
-  const query = 'INSERT INTO pallets (palletid, qty, partid, orderid) VALUES (?, ?, ?)';
+  const query = 'INSERT INTO pallets (palletid, qty, partid, orderid) VALUES ($1, $2, $3, $4)';
 
   const params = [palletName, palletQty, palletPart, 0];
 
-  return pgClient.execute(query, params, { prepare: true });
+  return pgClient.query(query, params);
 };
 
 module.exports = {
